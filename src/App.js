cae0ec9b355ffd03ab6faeca2554571cda4202c9
riyajawa <riya1119.cse19@chitkara.edu.in>
2021-06-02 15:37:53 +0530

    commit added

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Recipes from "./components/Recipes";
+import Axios from "axios";
+
+function App() {
+  const [search, setSerach] = useState("mango");
+  const [recipes, setRecipes] = useState([]);
+
+  const APP_ID = "84fc5d2b";
+  const APP_KEY = "f6b511668b14b931ed3047b5ee6f05ac";
+
+  const getRecipes = async () => {
+    const res = await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    setRecipes(res.data.hits);
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const onInputChange = (e) => {
+    setSerach(e.target.value);
+  };
+
+  const onSearchClick = () => {
+    getRecipes();
+  };
+  return (
+    <div className="App">
+      <Header
+        search={search}
+        onInputChange={onInputChange}
+        onSearchClick={onSearchClick}
+      />
+      <div className="container">
+        <Recipes recipes={recipes} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
